81341c83c60e6fe36ce5675a945b891338125d50
verizm <myemail@mail.ru>
2024-11-22 15:16:46 +0300

    fix names

--- purchase_counter.py
+++ purchase_counter.py
@@ -8,44 +8,43 @@ class PurchaseCounter:
 
     def __init__(self, path_to_file):
         self._path_to_file = path_to_file
-        self._total_by_purchases = {}
+        self._total_amount_by_each_purchase = {}
 
     def _add_purchase_to_total_by_count(self, purchase_name: str, purchase_count: int):
-        if purchase_name not in self._total_by_purchases:
-            self._total_by_purchases[purchase_name] = purchase_count
+        if purchase_name not in self._total_amount_by_each_purchase:
+            self._total_amount_by_each_purchase[purchase_name] = purchase_count
         else:
-            self._total_by_purchases[purchase_name] += purchase_count
+            self._total_amount_by_each_purchase[purchase_name] += purchase_count
 
-    def _parse_row_to_purchase_data(self, row: str):
+    @staticmethod
+    def _parse_row_to_purchase_data(row: str):
         purchase_data = [item.strip() for item in row.split(":")]
         if len(purchase_data) != 2:
             raise ParseRowToPurchaseDataError
         return purchase_data
 
-    def _convert_purchase_count_to_integer(self, purchase_count: str) -> int:
+    @staticmethod
+    def _convert_purchase_count_to_integer(purchase_count: str) -> int:
         if not purchase_count.isdigit():
             raise PurchaseCountIsNotIntegerError
         return int(purchase_count)
 
-    def _calculate_total_count_purchases(self):
+    def _calculate_total_amount_by_each_purchase(self):
         with open(self._path_to_file, "r") as file_as_purchase_data:
             for row in file_as_purchase_data:
                 try:
                     purchase_name, purchase_count = self._parse_row_to_purchase_data(row)
                     purchase_count_as_int = self._convert_purchase_count_to_integer(purchase_count)
+
                     self._add_purchase_to_total_by_count(purchase_name, purchase_count_as_int)
                 except (ParseRowToPurchaseDataError, PurchaseCountIsNotIntegerError):
                     continue
 
     def _print_total_by_purchases(self):
         print("Итог по всем покупкам:")
-        for purchase, count in self._total_by_purchases.items():
+        for purchase, count in self._total_amount_by_each_purchase.items():
             print(f"{purchase}: {count}")
 
-    def report_total_count_purchases(self):
-        self._calculate_total_count_purchases()
+    def report_total_amount_by_each_purchase(self):
+        self._calculate_total_amount_by_each_purchase()
         self._print_total_by_purchases()
-
-counter = PurchaseCounter("data.txt")
-counter._calculate_total_count_purchases()
-print(counter._total_by_purchases)
--- test_purchase_counter.py
+++ test_purchase_counter.py
@@ -28,15 +28,15 @@ class TestPurchaseCounter:
 
     def test_calculate_total_count_by_purchases(self):
         purchase_counter = PurchaseCounter("data.txt")
-        purchase_counter._calculate_total_count_purchases()
-        assert purchase_counter._total_by_purchases == {'apple': 13, 'banana': 7, 'grape': 6}
+        purchase_counter._calculate_total_amount_by_each_purchase()
+        assert purchase_counter._total_amount_by_each_purchase == {'apple': 13, 'banana': 7, 'grape': 6}
 
     def test_calculate_total_count_by_purchases_when_file_has_empty_lines(self):
         purchase_counter = PurchaseCounter("data_with_empty_lines.txt")
-        purchase_counter._calculate_total_count_purchases()
-        assert purchase_counter._total_by_purchases == {'apple': 13, 'banana': 7, 'grape': 6}
+        purchase_counter._calculate_total_amount_by_each_purchase()
+        assert purchase_counter._total_amount_by_each_purchase == {'apple': 13, 'banana': 7, 'grape': 6}
 
     def test_calculate_total_count_by_purchases_when_file_has_only_float_integers(self):
         purchase_counter = PurchaseCounter("data_with_float.txt")
-        purchase_counter._calculate_total_count_purchases()
-        assert purchase_counter._total_by_purchases == {}
+        purchase_counter._calculate_total_amount_by_each_purchase()
+        assert purchase_counter._total_amount_by_each_purchase == {}
